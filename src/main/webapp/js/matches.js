6a9525f9f38a82d9a6668eaf61117ba5a5fb30b3
Maxime Gréau <contact@mgreau.com>
2013-10-31 10:30:21 +0100

    dev JS multiples matches

--- src/main/webapp/js/matches.js
+++ src/main/webapp/js/matches.js
@@ -1,6 +1,6 @@
-var app = angular.module('app', []);
+var myApp = angular.module('myApp', []);
 
-app.factory('MatchService', function() {
+myApp.factory('MatchService', function() {
 	var service = {};
 
 	service.connect = function(idMatch) {
@@ -8,7 +8,7 @@ app.factory('MatchService', function() {
 			return;
 		}
 
-		var wsUrl = 'ws://' + window.location.host + window.location.pathname
+		var wsUrl = 'ws://localhost:8080/usopen/'
 				+ 'matches/' + idMatch;
 		var ws = new WebSocket(wsUrl);
 
@@ -18,56 +18,68 @@ app.factory('MatchService', function() {
 
 		ws.onerror = function() {
 			service.callback("Failed to open a connection");
-		}
+		};
 
 		ws.onclose = function() {
 			service.callback("Disconnected to livemode");
-		}
+		};
 
 		ws.onmessage = function(message) {
+			console.log(message);
 			service.callback(message.data);
 		};
 
 		service.ws = ws;
-	}
+	};
 
 	service.send = function(message) {
 		service.ws.send(message);
-	}
+	};
+	
+	//Close the WebSocket connection
+	service.disconnect = function() {
+		service.ws.close();
+	};
 
+	
 	service.subscribe = function(callback) {
 		service.callback = callback;
-	}
+	};
 
 	return service;
 });
 
 function AppCtrl($scope, MatchService, $http) {
 	$scope.messages = [];
-
+	
 	MatchService.subscribe(function(message) {
-		$scope.messages.push(message);
-		$scope.$apply();
-	});
+	    //$scope.messages.push(message);
+	    $scope.$apply();
+	  });
 
-	$scope.connect = function(key) {
-		MatchService.connect(key);
+	$scope.connect = function(id) {
+		MatchService.connect(id);
+	};
+	
+	$scope.disconnect = function() {
+		MatchService.disconnect();
 	};
 
-	$scope.send = function() {
-		MatchService.send($scope.text);
+
+	$scope.betOnWinner = function() {
+		var msg = "{ \"type\" : \"betMatchWinner\", \"name\" : \"" + $scope.text + "\" }";
+		MatchService.send(msg);
 		$scope.text = "";
 	};
 
 	$scope.splitForImage = function(string, nb) {
 		$scope.array = string.toLowerCase().split(' ');
-
 		return $scope.result = $scope.array[nb];
 	};
 
 	$http({
 		method : 'GET',
-		url : '/wildfly-websocket/rest/tournament/lives'
+		url : '/usopen/rest/tournament/lives'
 	}).success(function(data) {
 		$scope.matches = data; // response data
 	});
